b81147dbaeaab6cd60e6bd7813c38b8336e6a715
Diogo Osório <diogo.g.osorio@gmail.com>
2021-03-24 18:01:25 +0000

    Integrates material-ui

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+import { createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    text: {
+      primary: '#001122',
+    },
+  },
+  typography: {
+    htmlFontSize: 14,
+    fontFamily: `"Lato", "Lucida Grande", sans-serif`,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+
+    // used by material-ui on the CssBaseLine component as the default
+    // body text style
+    body2: {
+      fontSize: "1rem",
+      lineHeight: 1.714,
+    }
+  }
+})
+
+export default theme
